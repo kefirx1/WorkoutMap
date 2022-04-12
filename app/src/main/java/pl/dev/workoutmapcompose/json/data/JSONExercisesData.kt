eeb7987d5d318f91973af53445144683dfb9fb1e
kefirx1 <blazejkwiatkowski02@gmail.com>
2022-04-12 16:45:08 +0200

    JSON data and listOfExercises.json

---
+++ app/src/main/java/pl/dev/workoutmapcompose/json/data/JSONExercisesData.kt
@@ -0,0 +1,12 @@
+package pl.dev.workoutmapcompose.json.data
+
+data class JSONExercisesData(
+    val back: List<String>,
+    val belly: List<String>,
+    val biceps: List<String>,
+    val chest: List<String>,
+    val forearms: List<String>,
+    val legs: List<String>,
+    val shoulders: List<String>,
+    val triceps: List<String>
+)
