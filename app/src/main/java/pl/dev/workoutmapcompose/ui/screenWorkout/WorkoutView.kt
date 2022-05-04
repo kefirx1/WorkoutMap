0de666c81b06ee4b341d280de3e1ef9ef430264a
kefirx1 <blazejkwiatkowski02@gmail.com>
2022-05-04 22:06:58 +0200

    WorkoutView theme adaptation

--- app/src/main/java/pl/dev/workoutmapcompose/ui/screenWorkout/WorkoutView.kt
+++ app/src/main/java/pl/dev/workoutmapcompose/ui/screenWorkout/WorkoutView.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,7 +33,7 @@ fun MainWorkout(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(BlueGray900)
+            .background(MaterialTheme.colors.background)
             .padding(10.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
